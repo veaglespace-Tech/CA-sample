fad95b1115da4737870ef4e9d4a94666bafaf72b
Poteritesh <riteshpote0603@gmail.com>
2026-07-01 18:26:22 +0530

    style: change BrandLogo default text color to text-black

--- client/src/components/layout/BrandLogo.js
+++ client/src/components/layout/BrandLogo.js
@@ -42,7 +42,7 @@ export default function BrandLogo({
       </span>
       {showText && (
         <span className={`flex flex-col leading-none`}>
-          <span className={`text-[1.05rem] font-black tracking-[-0.04em] sm:text-[1.2rem] ${wordmarkClassName || 'text-slate-900'}`}>
+          <span className={`text-[1.05rem] font-black tracking-[-0.04em] sm:text-[1.2rem] ${wordmarkClassName || 'text-black'}`}>
             Veagle <span className="text-sky-500">Space</span>
           </span>
         </span>
